bfb0f391dcc94aa44bed7478f9c3de1403bb30b4
EDG-136 <booboo12keys@gmail.com>
2024-10-03 19:07:41 -0700

    Update inputHandler.java

--- src/Controller/inputHandler.java
+++ src/Controller/inputHandler.java
@@ -1,8 +1,6 @@
 package Controller;
 
-import View.Renderer;
-import Model.Mesh;
-import Model.Camera;
+
 import View.Window;
 import org.lwjgl.glfw.GLFW;
 import org.joml.Vector3f;
@@ -12,11 +10,8 @@ import java.awt.*;
 import static org.lwjgl.glfw.GLFW.*;
 
 
-public class TECK_Engine {
+public class inputHandler {
     private Window window;
-    private Renderer renderer;
-    private Mesh mesh;
-    private Camera camera;
 
     float[] vertices = {
             0.5f,  0.5f, 0.0f,  // top right
@@ -34,15 +29,10 @@ public class TECK_Engine {
     };
 
     public void init() {
-        window = new Window(800, 600, "3D Engine");
+        window = new Window(800, 600, "myPath");
         window.init();
 
-        renderer = new Renderer();
-        renderer.init();
 
-        mesh = new Mesh(vertices, indices); // Load mesh with data
-
-        camera = new Camera();
     }
 
     public void run() {
@@ -50,7 +40,7 @@ public class TECK_Engine {
             window.pollEvents();
 
             processInput();
-            renderer.render(mesh, camera);
+
 
             window.update();
         }
@@ -60,14 +50,14 @@ public class TECK_Engine {
 
     public void processInput() {
         if (glfwGetKey(window.getWindowID(), GLFW_KEY_W) == GLFW_PRESS) {
-            camera.move(new Vector3f(1, 0, -1f));
+            //camera.move(new Vector3f(1, 0, -1f));
         }
         // Handle more inputs here
     }
 
     public void cleanup() {
-        renderer.cleanup();
-        mesh.cleanup();
+       // renderer.cleanup();
+       // mesh.cleanup();
         window.cleanup();
     }
 
