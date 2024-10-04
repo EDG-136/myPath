c345f949883cc74075bbd0bc9fb1d8d07f7f4834
EDG-136 <booboo12keys@gmail.com>
2024-10-03 19:07:55 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import Controller.inputHandler;
+
 public class Main{
+
   public static void main(String[] args){
 
     System.out.println("Hello World!");
+    inputHandler instance = new inputHandler();
+
+    instance.init();
+    instance.run();
+
   }
 
 }
