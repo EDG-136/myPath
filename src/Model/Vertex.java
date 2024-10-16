dfa5c9577b24e7c562a23f029b294553ab77d885
EDG-136 <booboo12keys@gmail.com>
2024-10-15 17:00:06 -0700

    Update Vertex.java

--- src/Model/Vertex.java
+++ src/Model/Vertex.java
@@ -1,1 +1,20 @@
+package Model;
+
+import org.joml.Vector3f;
+
+public class Vertex {
+
+    public Vector3f position;
+    public Vector3f normal;
+    public Vector3f color;
+    public Vector3f texUV;
+
+    public Vertex(Vector3f p, Vector3f n, Vector3f c, Vector3f uv){
+
+        this.position = p;
+        this.normal = n;
+        this.color = c;
+        this.texUV =uv;
+    }
+}
 
