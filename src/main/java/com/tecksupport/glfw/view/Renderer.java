9648c1f4165d3c4a04263c7a14d8346e06b5e831
EDG-136 <booboo12keys@gmail.com>
2024-10-22 12:20:17 -0700

    Update Renderer.java
    what does the drawing of the VAO's

--- src/main/java/com/tecksupport/glfw/view/Renderer.java
+++ src/main/java/com/tecksupport/glfw/view/Renderer.java
@@ -1,1 +1,23 @@
 package com.tecksupport.glfw.view;
+
+import com.tecksupport.glfw.model.RawModel;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+
+public class Renderer{
+
+    public void prepare(){
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(1,0,0,1);
+    }
+    public void render(RawModel model){
+        glBindVertexArray(model.getVaoID());
+        glEnableVertexAttribArray(0);
+        glDrawElements(GL_TRIANGLES, model.getVertexCount(), GL_UNSIGNED_INT, 0);
+        glDisableVertexAttribArray(0);
+        glBindVertexArray(0);
+    }
+}
