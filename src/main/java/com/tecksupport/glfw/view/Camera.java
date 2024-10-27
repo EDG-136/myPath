7a13a4ccff3939e7992f730c02209df205e5a9f3
EDG-136 <booboo12keys@gmail.com>
2024-10-26 23:26:45 -0700

    Update Camera.java

--- src/main/java/com/tecksupport/glfw/view/Camera.java
+++ src/main/java/com/tecksupport/glfw/view/Camera.java
@@ -1,17 +1,23 @@
 package com.tecksupport.glfw.view;
 
+import com.tecksupport.glfw.model.Shader;
+import org.joml.Math;
+import org.joml.Matrix4f;
 import org.joml.Vector3f;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.system.MemoryStack;
+import java.nio.FloatBuffer;
+
+import static org.lwjgl.opengl.GL20.*;
 
 public class Camera {
 
-    private final float MOVE_SPEED = 1f;
-    private final Vector3f position = new Vector3f(0, 0, 0);
+    private Vector3f position = new Vector3f(0,0,0);
     private float pitch;
     private float yaw;
     private float roll;
 
-    public Camera() {
-    }
+    public Camera(){}
 
     public Vector3f getPosition() {
         return position;
@@ -31,28 +37,21 @@ public class Camera {
         return roll;
     }
 
-    public void forward() {
-        position.z -= MOVE_SPEED;
+    public void forward(){
+        position.z-=0.2f;
+    }
+    public void left(){
+        position.x-=0.2f;
+    }
+    public void right(){
+        position.x+=0.2f;
+    }
+    public void backward(){
+        position.z+=0.2f;
     }
 
-    public void left() {
-        position.x -= MOVE_SPEED;
-    }
+    public void up(){position.y += 0.2f;}
+    public void down(){position.y -= 0.2f;}
 
-    public void right() {
-        position.x += MOVE_SPEED;
-    }
 
-    public void backward() {
-        position.z += MOVE_SPEED;
-    }
-
-    public void down() {
-        position.y -= MOVE_SPEED;
-        
-    }
-
-    public void up() {
-        position.y += MOVE_SPEED;
-    }
 }
