cb23ef7d0ea567fd108f0a6ef74eced444999287
EDG-136 <booboo12keys@gmail.com>
2024-10-27 19:25:57 -0700

    better controls

--- src/main/java/com/tecksupport/glfw/controller/InputHandler.java
+++ src/main/java/com/tecksupport/glfw/controller/InputHandler.java
@@ -163,10 +163,10 @@ public class InputHandler {
         if (glfwGetKey(window.getWindowID(), GLFW_KEY_D) == GLFW_PRESS) {
             camera.right();
         }
-        if (glfwGetKey(window.getWindowID(), GLFW_KEY_E) == GLFW_PRESS) {
+        if (glfwGetKey(window.getWindowID(), GLFW_KEY_SPACE) == GLFW_PRESS) {
             camera.up();
         }
-        if (glfwGetKey(window.getWindowID(), GLFW_KEY_Q) == GLFW_PRESS) {
+        if (glfwGetKey(window.getWindowID(), GLFW_KEY_LEFT_ALT) == GLFW_PRESS) {
             camera.down();
         }
         // Handle more inputs here
